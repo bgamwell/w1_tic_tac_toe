998b8cd6d7df478882a6474faeac1750cca40802
bgamwell <bgamwell@gmail.com>
2015-06-28 20:24:06 -0700

    First version of tic-tac-toe; needs updates to user interface and styles

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,127 +1,92 @@
 // wait for the DOM to finish loading
+
 window.addEventListener("DOMContentLoaded", function() {
 
-  alert("Your javascript works!");
-
-  //Variables
-
-  var row1col1 = document.getElementById("row1col1");
-  var row1col2 = document.getElementById("row1col2");
-  var row1col3 = document.getElementById("row1col3");
-  var row2col1 = document.getElementById("row2col1");
-  var row2col2 = document.getElementById("row2col2");
-  var row2col3 = document.getElementById("row2col3");
-  var row3col1 = document.getElementById("row3col1");
-  var row3col2 = document.getElementById("row3col2");
-  var row3col3 = document.getElementById("row3col3");
+//  Variables
 
   var allBoxes = document.querySelectorAll(".box");
 
   var resetButton = document.querySelector(".btn");
 
-  //Functions
+  var headline = document.getElementById("headline");
 
-  var playerTurn == "x";
+  var playerXScorecard = document.getElementById("playerxscorecard");
 
-  if (playerTurn == "x") {
+  var playerYScorecard = document.getElementById("playeryscorecard");
 
+  var playerTurn = "X";
 
-  }
+  var playerXScore = 0;
 
-  resetButton.onclick = function(event){
+  var playerYScore = 0;
+
+  // Reset Button
+
+  var resetBoard = resetButton.onclick = function(event){
     for (i = 0; i < allBoxes.length; i++) {
         allBoxes[i].style.backgroundColor = "white";
-      }
+        allBoxes[i].innerText = "";
+      };
+      playerTurn = "X";
+      console.log(playerTurn);
   };
 
-  row1col1.onclick = function(event){
-    row1col1.style.backgroundColor = "green";
+  function checkForChampion() {
+    if (playerXScore === 3) {
+      alert("X wins three times! Congrats, you're the champion!");
+      window.location.reload();
+    } else if (playerYScore === 3) {
+      alert("O wins three times! Congrats, you're the champion!");
+      window.location.reload();
+    };
   };
 
-  row1col1.ondblclick = function(event){
-    row1col1.style.backgroundColor = "yellow";
+  // Pick Winner
+
+  function checkForWinner() {
+    if ( row1col1.innerText === "X" && row1col2.innerText === "X" && row1col3.innerText === "X" || row2col1.innerText === "X" && row2col2.innerText === "X" && row2col3.innerText === "X" || row3col1.innerText === "X" && row3col2.innerText === "X" && row3col3.innerText === "X" || row1col1.innerText === "X" && row2col1.innerText === "X" && row3col1.innerText === "X" || row2col1.innerText === "X" && row2col2.innerText === "X" && row2col3.innerText === "X" || row3col1.innerText === "X" && row3col2.innerText === "X" && row3col3.innerText === "X" || row1col1.innerText === "X" && row2col2.innerText === "X" && row3col3.innerText === "X" || row1col3.innerText === "X" && row2col2.innerText === "X" && row3col1.innerText == "X" )  {
+    alert("X wins this round!");
+    playerXScore++;
+    playerXScorecard.innerText = playerXScore.toString();
+    checkForChampion();
+    resetBoard();
+    } else if (row1col1.innerText === "O" && row1col2.innerText === "O" && row1col3.innerText === "O" || row2col1.innerText === "O" && row2col2.innerText === "O" && row2col3.innerText === "O" || row3col1.innerText === "O" && row3col2.innerText === "O" && row3col3.innerText === "O" || row1col1.innerText === "O" && row2col1.innerText === "O" && row3col1.innerText === "O" || row2col1.innerText === "O" && row2col2.innerText === "O" && row2col3.innerText === "O" || row3col1.innerText === "O" && row3col2.innerText === "O" && row3col3.innerText === "O" || row1col1.innerText === "O" && row2col2.innerText === "O" && row3col3.innerText === "O" || row1col3.innerText === "O" && row2col2.innerText === "O" && row3col1.innerText == "O" ) {
+    alert("O wins this round!")
+    playerYScore++;
+    playerYScorecard.innerText = playerYScore.toString();
+    checkForChampion();
+    resetBoard();
+    } else {
+      switchTurn();
+    };
   };
 
-  row1col2.onclick = function(event){
-    row1col2.style.backgroundColor = "green";
+  function switchTurn() {
+    if (playerTurn === "X") {
+      playerTurn = "O";
+    } else {
+      playerTurn = "X";
+    };
   };
 
-  row1col2.ondblclick = function(event){
-    row1col2.style.backgroundColor = "yellow";
+// Adds an event listener to all boxes
+
+  for (i = 0; i < allBoxes.length; i++) {
+    allBoxes[i].onclick = function(event) {
+      if (this.innerText === "") {
+        if (playerTurn === "X") {
+          this.style.backgroundColor = "green";
+          this.innerText = playerTurn;
+          checkForWinner();
+        } else {
+          this.style.backgroundColor = "yellow";
+          this.innerText = playerTurn;
+          checkForWinner();
+        };
+      } else {
+        alert("This square has already been played!")
+      };
+    };
   };
-
-  row1col3.onclick = function(event){
-    row1col3.style.backgroundColor = "green";
-  };
-
-  row1col3.ondblclick = function(event){
-    row1col3.style.backgroundColor = "yellow";
-  };
-
-  row2col1.onclick = function(event){
-    row2col1.style.backgroundColor = "green";
-  };
-
-  row2col1.ondblclick = function(event){
-    row2col1.style.backgroundColor = "yellow";
-  };
-
-  row2col2.onclick = function(event){
-    row2col2.style.backgroundColor = "green";
-  };
-
-  row2col2.ondblclick = function(event){
-    row2col2.style.backgroundColor = "yellow";
-  };
-
-  row2col3.onclick = function(event){
-    row2col3.style.backgroundColor = "green";
-  };
-
-  row2col3.ondblclick = function(event){
-    row2col3.style.backgroundColor = "yellow";
-  };
-
-  row3col1.onclick = function(event){
-    row3col1.style.backgroundColor = "green";
-  };
-
-  row3col1.ondblclick = function(event){
-    row3col1.style.backgroundColor = "yellow";
-  };
-
-  row3col2.onclick = function(event){
-    row3col2.style.backgroundColor = "green";
-  };
-
-  row3col2.ondblclick = function(event){
-    row3col2.style.backgroundColor = "yellow";
-  };
-
-  row3col3.onclick = function(event){
-    row3col3.style.backgroundColor = "green";
-  };
-
-  row3col3.ondblclick = function(event){
-    row3col3.style.backgroundColor = "yellow";
-  };
-
-
-
-  // document.addEventListener("click", function() {
-  //   row1col1.style.backgroundColor = "green";
-  // });
-
-  //
-  // document.addEventListener("click", function() {
-  //   for (i = 0; i < allBoxes.length; i++) {
-  //   allBoxes[i].style.backgroundColor = "white";
-  //   }
-  // });
-
-
-
-
-
 
 }); //close window event listener
